e10589e7089f6a1c09937aa4ba49ce8e4d171381
Om1603 <82240153+Om1603@users.noreply.github.com>
2022-09-20 22:07:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
